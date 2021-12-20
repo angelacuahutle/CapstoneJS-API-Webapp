a7ed4ea675802cd56b8a75c706a995bd4698d597
angelacuahutle <angela.cuahutle@gmail.com>
2021-12-20 16:56:31 -0600

    Add files and start intallation of package json

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import printMe from './print.js';
+import './style.css';
+
+function component() {
+  
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
