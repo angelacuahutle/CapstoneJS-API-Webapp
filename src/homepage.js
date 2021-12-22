7fbc168382934bc4956b339a3d956dc8d82df7a9
angelacuahutle <angela.cuahutle@gmail.com>
2021-12-22 17:02:53 -0600

    Create post likes with class and fucntion

--- src/homepage.js
+++ src/homepage.js
@@ -1,57 +1,66 @@
+import { DataAPI } from './Involvement_API';
 const PokeCards = document.getElementById('pokeCards');
 
 const createPokeCard = (pokemon) => {
-  const cardContainer = document.createElement('div');
-  const card = document.createElement('div');
-  const imgContainer = document.createElement('img');
-  const cardBody = document.createElement('div');
-  const cardTitle = document.createElement('h5');
-  const likeIconContainer = document.createElement('a');
-  const likeIcon = document.createElement('i');
-  const likeCount = document.createElement('span');
-  const cardText = document.createElement('p');
-  const commentButton = document.createElement('a');
-  cardContainer.classList.add('col-12', 'col-sm-6', 'col-md-3', 'pt-4');
-  card.classList.add('card', 'border', 'border-2', 'border-black', 'card-style');
-  imgContainer.classList.add('card-img-top');
-  imgContainer.alt = `Pokemon Card image of ${pokemon.name}`;
-  imgContainer.src = pokemon.images.small;
-  cardBody.classList.add('card-body', 'row', 'justify-content-center', 'align-items-center');
-  cardTitle.classList.add('card-title', 'pb-2', 'col-6', 'd-flex', 'justify-content-center', 'align-items-center');
-  cardTitle.innerText = pokemon.name;
-  likeIconContainer.classList.add('col-4', 'pb-2', 'd-flex', 'justify-content-center', 'align-items-center', 'iconLike');
-  likeIcon.classList.add('fas', 'fa-heart', 'fa-2x', 'col-4', 'd-flex', 'justify-content-center', 'align-items-center');
-  likeCount.classList.add('col-2', 'd-flex', 'justify-content-center', 'align-items-center');
-  likeCount.innerText = 0;
-  cardText.classList.add('card-text', 'd-flex', 'justify-content-evenly', 'align-items-center');
-  cardText.innerText = pokemon.flavorText || 'No Description';
-  commentButton.classList.add('btn', 'btn-primary', 'col-6');
-  commentButton.innerText = 'Comments';
+    const cardContainer = document.createElement('div');
+    const card = document.createElement('div');
+    const imgContainer = document.createElement('img');
+    const cardBody = document.createElement('div');
+    const cardTitle = document.createElement('h5');
+    const likeIconContainer = document.createElement('a');
+    const likeIcon = document.createElement('i');
+    const likeCount = document.createElement('span');
+    const cardText = document.createElement('p');
+    const commentButton = document.createElement('a');
+    cardContainer.classList.add('col-12', 'col-sm-6', 'col-md-3', 'pt-4');
+    card.classList.add('card', 'border', 'border-2', 'border-black', 'card-style');
+    imgContainer.classList.add('card-img-top');
+    imgContainer.alt = `Pokemon Card image of ${pokemon.name}`;
+    imgContainer.src = pokemon.images.small;
+    cardBody.classList.add('card-body', 'row', 'justify-content-center', 'align-items-center');
+    cardTitle.classList.add('card-title', 'pb-2', 'col-6', 'd-flex', 'justify-content-center', 'align-items-center');
+    cardTitle.innerText = pokemon.name;
+    likeIconContainer.classList.add('col-4', 'pb-2', 'd-flex', 'justify-content-center', 'align-items-center', 'iconLike');
+    likeIcon.classList.add('fas', 'fa-heart', 'fa-2x', 'col-4', 'd-flex', 'justify-content-center', 'align-items-center');
+    likeCount.classList.add('col-2', 'd-flex', 'justify-content-center', 'align-items-center');
+    likeCount.innerText = 0;
+    cardText.classList.add('card-text', 'd-flex', 'justify-content-evenly', 'align-items-center');
+    cardText.innerText = pokemon.flavorText || 'No Description';
+    commentButton.classList.add('btn', 'btn-primary', 'col-6');
+    commentButton.innerText = 'Comments';
 
-  likeIconContainer.appendChild(likeIcon);
-  cardBody.appendChild(cardTitle);
-  cardBody.appendChild(likeIconContainer);
-  cardBody.appendChild(likeCount);
-  cardBody.appendChild(cardText);
-  cardBody.appendChild(commentButton);
-  card.appendChild(imgContainer);
-  card.appendChild(cardBody);
-  cardContainer.appendChild(card);
-  PokeCards.appendChild(cardContainer);
-  /* cardContainer.addEventListener('click', () => {
-        const modal = document.getElementById('modal');
-        const modalImg = document.getElementById('modal-img');
-        const modalTitle = document.getElementById('modal-title');
-        const modalText = document.getElementById('modal-text');
-        const modalClose = document.getElementById('modal-close');
-        modal.style.display = 'block';
-        modalImg.src = pokemon.images.large;
-        modalTitle.innerText = pokemon.name;
-        modalText.innerText = pokemon.text;
-        modalClose.addEventListener('click', () => {
-            modal.style.display = 'none';
-        })
-    } */
+    likeIconContainer.appendChild(likeIcon);
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(likeIconContainer);
+    cardBody.appendChild(likeCount);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(commentButton);
+    card.appendChild(imgContainer);
+    card.appendChild(cardBody);
+    cardContainer.appendChild(card);
+    PokeCards.appendChild(cardContainer);
+    /* cardContainer.addEventListener('click', () => {
+          const modal = document.getElementById('modal');
+          const modalImg = document.getElementById('modal-img');
+          const modalTitle = document.getElementById('modal-title');
+          const modalText = document.getElementById('modal-text');
+          const modalClose = document.getElementById('modal-close');
+          modal.style.display = 'block';
+          modalImg.src = pokemon.images.large;
+          modalTitle.innerText = pokemon.name;
+          modalText.innerText = pokemon.text;
+          modalClose.addEventListener('click', () => {
+              modal.style.display = 'none';
+          })
+      } */
+
+    likeIconContainer.addEventListener('click', () => {
+        DataAPI.microverseInvolvement.postLike(pokemon.id).then((data) => {
+            console.log(data);
+        });
+    });
 };
 
-export { createPokeCard }; // eslint-disable-line import/prefer-default-export
+export {
+    createPokeCard
+}; // eslint-disable-line import/prefer-default-export
--- src/index.js
+++ src/index.js
@@ -5,64 +5,12 @@ import '@fortawesome/fontawesome-free/js/brands';
 import './css/bootstrap.min.css';
 import './css/style.css';
 
+import { PokemonAPI } from './PokeTCG_API';
+import { createPokeCard } from './homepage';
 
-class Inv_API {
-  InvolvementURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/jO96LQf9ZZkhY1OlDSZo/likes';
-  API_KEY = 'jO96LQf9ZZkhY1OlDSZo';
-  baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
-
-    passOptions = (objOption) => {
-      const options = {
-        method: `${objOption}`,
-        bodyObj
-        headers: {
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        headers: {
-          'Content-Type': `application/json`,
-        },
-      };
-      return options;
-    };
-
-    basicCall2Api = async (method, query, bodyObj) => {
-      const url = this.Inv_API.baseURL + this.Inv_API.API_KEY + '/' + query;
-      console.log(url);
-      const options = this.Inv_API.passOptions(method);
-      console.log(bodyObj);
-      options.body = bodyObj;
-      //options.body = JSON.stringify(bodyObj);
-      console.log(options);
-      const response = await fetch(url, options);
-      if (method === 'POST') {
-        console.log('Entering POST');
-        return response.text();
-      } else {
-        console.log('Another method');
-        return response.json();
-      }
-    };
-
-  getLike = () => {
-      this.Inv_API.basicCall2Api('GET', 'likes');
-    },
-    postLike = (cardId) => {
-      const bodyObj = {
-        item_id: cardId,
-      };
-      return this.Inv_API.basicCall2Api('POST', 'likes', bodyObj);
-    };
-
-    getComments = (cardId) => this.Inv_API.basicCall2Api('GET', `comments?item_id=${cardId}`),
-
-    postComment = (commentObj) => this.Inv_API.basicCall2Api('POST', 'comments', commentObj),
-  }
-}
-
-const InvAPI = new Inv_API();
-export {
-  InvAPI
-};
-
-//  getCardSetSwShbyId: (cardId) => this.TCGpokemon.basicCall2Api(`id:${cardId}`),
-//  getTwelveCardsSwSh: () => this.TCGpokemon.basicCall2Api('set.id:swsh1&pageSize=12'),
+PokemonAPI.TCGpokemon.getTwelveCardsSwSh().then((data) => {
+  console.log(data);
+  data.data.forEach((pokemon) => {
+    createPokeCard(pokemon);
+  });
+});
