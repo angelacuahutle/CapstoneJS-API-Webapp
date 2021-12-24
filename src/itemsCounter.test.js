a882437cd24085ae87665564a0c509f946e6f4e8
TheLuisAbarca <arclaav.adm@gmail.com>
2021-12-24 09:34:47 -0500

    Fix path to for the test file.

--- src/itemsCounter.test.js
+++ src/itemsCounter.test.js
@@ -1,16 +1,16 @@
 import { countNumberOfCards } from './homepage';
 
 describe('CountItems', () => {
-    test('Counting Elements added to the DOM', () => {
-        const basicBody = document.createElement('div');
-        const pokeCard1 = document.createElement('div');
-        const pokeCard2 = document.createElement('div');
-        pokeCard1.classList.add('card');
-        pokeCard2.classList.add('card');
-        basicBody.appendChild(pokeCard1);
-        basicBody.appendChild(pokeCard2);
-        document.body.appendChild(basicBody);
-        const numberOfCards = countNumberOfCards();
-        expect(numberOfCards).toBe(2);
-    })
+  test('Counting Elements added to the DOM', () => {
+    const basicBody = document.createElement('div');
+    const pokeCard1 = document.createElement('div');
+    const pokeCard2 = document.createElement('div');
+    pokeCard1.classList.add('card');
+    pokeCard2.classList.add('card');
+    basicBody.appendChild(pokeCard1);
+    basicBody.appendChild(pokeCard2);
+    document.body.appendChild(basicBody);
+    const numberOfCards = countNumberOfCards();
+    expect(numberOfCards).toBe(2);
+  });
 });
